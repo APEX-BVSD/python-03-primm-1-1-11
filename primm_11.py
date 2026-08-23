20d8d43eed0dbe03d4f55aeb50cbb208df90e0e9
Jon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2026-08-23 12:55:28 -0600

    Initial commit

---
+++ primm_11.py
@@ -0,0 +1,16 @@
+"""
+PRIMM: 1+1 = 11
+Description of program here
+Name - Date
+"""
+
+def main():
+  
+    num1: int = input("Enter a number: ")
+    num2: int = input("Enter another number: ")
+    result: int = num1 + num2
+
+    print(f"{num1} + {num2} = {result}")
+
+if __name__ == "__main__":
+  main()
